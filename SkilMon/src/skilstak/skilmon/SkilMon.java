ae8bcab2fdd2a0292c06cd5f0a8ac6900be459e7
gorroth1 <gorroth1@gmail.com>
2015-09-25 19:01:29 -0400

    save

---
+++ SkilMon/src/skilstak/skilmon/SkilMon.java
@@ -0,0 +1,16 @@
+package skilstak.skilmon;
+
+import java.util.ArrayList;
+
+public final class SkilMon {
+    public int attack, defense, exp, 
+    happiness, hp, spAttack, spDefense, Speed, total, 
+    evolvesAt;
+    
+    public String species;
+   
+    public double catchRate, growhRate, height, width, mfRatio;
+    
+    public ArrayList<String> abilities, moves, types;
+
+}
